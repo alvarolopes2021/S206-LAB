d7418b5dbd542543d74a76fe49944b544d8f90cb
alvaro <alvarolopes2020@protonmail.com>
2022-11-08 22:55:40 -0300

    feat: testing mozilla website

--- exercicio-avaliativo/cypress/e2e/exercicio-avaliativo.cy.js
+++ exercicio-avaliativo/cypress/e2e/exercicio-avaliativo.cy.js
@@ -1,5 +1,13 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="cypress" />
+
+describe('Testing developer mozilla website', () => {
+
+  it('Changing theme', () => {
+
+    cy.viewport(1340, 750) // Set viewport to 550px x 750px
+    cy.visit('https://developer.mozilla.org/pt-BR/');
+    cy.get('div.theme-switcher-menu > .action > .button-wrap').click();
+    cy.get('.submenu > :nth-child(2) > .button > .button-wrap').click();
   })
+
 })
