3d33424bb6fb90ccbf519230bd537aaa98ac3453
alvaro <alvarolopes2020@protonmail.com>
2022-11-09 22:26:26 -0300

    feat: new test added

--- exercicio-avaliativo/cypress/e2e/exercicio-avaliativo.cy.js
+++ exercicio-avaliativo/cypress/e2e/exercicio-avaliativo.cy.js
@@ -3,7 +3,7 @@
 describe('Testing developer mozilla website', () => {
 
   it.skip('Changing theme', () => {
-    cy.viewport(1340, 750) // Set viewport to 550px x 750px
+    cy.viewport(1340, 750) 
     cy.visit('https://developer.mozilla.org/pt-BR/');
     cy.get('div.theme-switcher-menu > .action > .button-wrap').click();
     cy.get('.submenu > :nth-child(2) > .button > .button-wrap').click();
@@ -12,12 +12,21 @@ describe('Testing developer mozilla website', () => {
       .and('eq', 'rgb(27, 27, 27)');
   });
 
-  it('Searching', () => {
-    cy.viewport(1340, 750) // Set viewport to 550px x 750px
+  it.skip('Searching', () => {
+    cy.viewport(1340, 750)
     cy.visit('https://developer.mozilla.org/pt-BR/');
     cy.get('#hp-search-input').type('doctype');
     cy.get('#hp-search-form > .search-button').click();
     cy.get('h1').should('contain.text', 'Search results for: doctype')
-  })
+  }) 
+
+  it('Checking if the language is NOT portuguese', () => {
+    cy.viewport(1340, 750)
+    cy.visit('https://developer.mozilla.org/pt-BR/');
+    cy.get('.top-navigation-main :nth-child(1) > a.top-level-entry').click();
+    cy.get('#languages-switcher-button > .button-wrap').click();
+    cy.get('.submenu > :nth-child(2) > .button').click();
+    cy.get('#languages-switcher-button > .button-wrap').should('not.have.text', 'Portuguese')
+  }) 
 
 })
