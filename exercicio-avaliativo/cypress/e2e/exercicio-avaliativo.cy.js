87932d3c485141e882aab5cd72ee57764d507af4
alvaro <alvarolopes2020@protonmail.com>
2022-11-09 21:34:33 -0300

    chore: added main readme

--- exercicio-avaliativo/cypress/e2e/exercicio-avaliativo.cy.js
+++ exercicio-avaliativo/cypress/e2e/exercicio-avaliativo.cy.js
@@ -10,6 +10,16 @@ describe('Testing developer mozilla website', () => {
     cy.get('div.theme-switcher-menu > .action > .button-wrap').click();
     cy.get('.submenu > :nth-child(2) > .button > .button-wrap').should('have.css', 'background-color')
       .and('eq', 'rgb(27, 27, 27)');
+  });
+
+  it('Changing theme', () => {
+    cy.viewport(1340, 750) // Set viewport to 550px x 750px
+    cy.visit('https://developer.mozilla.org/pt-BR/');
+    cy.get('div.theme-switcher-menu > .action > .button-wrap').click();
+    cy.get('.submenu > :nth-child(2) > .button > .button-wrap').click();
+    cy.get('div.theme-switcher-menu > .action > .button-wrap').click();
+    cy.get('.submenu > :nth-child(2) > .button > .button-wrap').should('have.css', 'background-color')
+      .and('eq', 'rgb(27, 27, 27)');
   })
 
 })
