160fc8dc8bfef50735bc3327d4ff22f5c8bff05b
alvaro <alvarolopes2020@protonmail.com>
2022-11-09 21:58:43 -0300

    feat: new test added

--- exercicio-avaliativo/cypress/e2e/exercicio-avaliativo.cy.js
+++ exercicio-avaliativo/cypress/e2e/exercicio-avaliativo.cy.js
@@ -2,7 +2,7 @@
 
 describe('Testing developer mozilla website', () => {
 
-  it('Changing theme', () => {
+  it.skip('Changing theme', () => {
     cy.viewport(1340, 750) // Set viewport to 550px x 750px
     cy.visit('https://developer.mozilla.org/pt-BR/');
     cy.get('div.theme-switcher-menu > .action > .button-wrap').click();
@@ -12,14 +12,12 @@ describe('Testing developer mozilla website', () => {
       .and('eq', 'rgb(27, 27, 27)');
   });
 
-  it('Changing theme', () => {
+  it('Searching', () => {
     cy.viewport(1340, 750) // Set viewport to 550px x 750px
     cy.visit('https://developer.mozilla.org/pt-BR/');
-    cy.get('div.theme-switcher-menu > .action > .button-wrap').click();
-    cy.get('.submenu > :nth-child(2) > .button > .button-wrap').click();
-    cy.get('div.theme-switcher-menu > .action > .button-wrap').click();
-    cy.get('.submenu > :nth-child(2) > .button > .button-wrap').should('have.css', 'background-color')
-      .and('eq', 'rgb(27, 27, 27)');
+    cy.get('#hp-search-input').type('doctype');
+    cy.get('#hp-search-form > .search-button').click();
+    cy.get('h1').should('contain.text', 'Search results for: doctype')
   })
 
 })
